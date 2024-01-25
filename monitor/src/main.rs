f8546847ac149067c719d54bfc08655d335d3e04
Josh Seligman <joshuaseligman18@gmail.com>
2024-01-25 16:41:30 -0500

    Added logging for now, but want to look into the tracing library

--- monitor/src/main.rs
+++ monitor/src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+extern crate log;
+use log::info;
+
+extern crate tokio;
+use tokio::time::{interval, Duration};
+use tokio::task;
+
+extern crate uuid;
+use uuid::Uuid;
+
+fn test_monitor_func(run_uuid: Uuid) {
+    info!("{:?} - hello world", run_uuid); 
+}
+
+#[tokio::main]
+async fn main() {
+    env_logger::init();
+
+    let forever = task::spawn(async {
+        let mut interval = interval(Duration::from_secs(10));
+
+        loop {
+            interval.tick().await;
+            test_monitor_func(Uuid::new_v4());
+        }
+    });
+
+    let _ = forever.await;
 }
