47b04b2f348a86b5e196b94b3f08dc7ecbecc4ed
Josh Seligman <joshuaseligman18@gmail.com>
2024-01-28 11:25:26 -0500

    Added tracing instead of logging to add more context and information to our logs

--- monitor/src/main.rs
+++ monitor/src/main.rs
@@ -1,27 +1,40 @@
-extern crate log;
-use log::info;
-
 extern crate tokio;
-use tokio::time::{interval, Duration};
 use tokio::task;
+use tokio::time::{interval, Duration};
 
-extern crate uuid;
-use uuid::Uuid;
+extern crate tracing;
+use tracing::{event, instrument, span, Level, Span};
 
-fn test_monitor_func(run_uuid: Uuid) {
-    info!("{:?} - hello world", run_uuid); 
+extern crate tracing_subscriber;
+
+use std::io;
+
+#[instrument]
+fn test_monitor_func(id: tracing::Id) {
+    event!(Level::DEBUG, "hello world");
+    sub_func(id.into_u64() % 42);
+}
+
+#[instrument]
+fn sub_func(my_num: u64) {
+    event!(Level::INFO, "Inside the sub_func");
 }
 
 #[tokio::main]
 async fn main() {
-    env_logger::init();
+    tracing_subscriber::fmt()
+        .with_writer(io::stderr)
+        .with_max_level(Level::TRACE)
+        .init();
 
     let forever = task::spawn(async {
         let mut interval = interval(Duration::from_secs(10));
 
         loop {
             interval.tick().await;
-            test_monitor_func(Uuid::new_v4());
+            let monitor_span: Span = span!(Level::TRACE, "monitor");
+            let _guard = monitor_span.enter();
+            test_monitor_func(monitor_span.id().unwrap());
         }
     });
 
