21f8db60f17c19d1ae87dad0689129cb73f4f8b7
Josh Seligman <joshuaseligman18@gmail.com>
2024-02-06 15:12:52 -0500

    Set up basic tracing

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+use tokio::net::TcpListener;
+use tower_http::trace::TraceLayer;
+use tracing::Level;
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt()
+        .with_writer(std::io::stderr)
+        .with_max_level(Level::TRACE)
+        .init();
+
+    let app: Router = Router::new().route("/", get(|| async { "Hello world" }))
+        .layer(TraceLayer::new_for_http());
+    let listener: TcpListener = tokio::net::TcpListener::bind("0.0.0.0:4242").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
