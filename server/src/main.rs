b7fa71b402358207b596b1315e3eda3e27e0de10
Josh Seligman <joshuaseligman18@gmail.com>
2024-02-06 16:59:20 -0500

    Served the static webpage

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,7 +1,10 @@
-use axum::{routing::get, Router};
+use axum::Router;
 use tokio::net::TcpListener;
 use tower_http::trace::TraceLayer;
+use tower_http::services::ServeDir;
 use tracing::Level;
+
+const WEB_FILES_DIR: &str = "/var/nebula/web";
 
 #[tokio::main]
 async fn main() {
@@ -10,9 +13,12 @@ async fn main() {
         .with_max_level(Level::TRACE)
         .init();
 
+    let web_service: ServeDir = ServeDir::new(WEB_FILES_DIR);
+
     let app: Router = Router::new()
-        .route("/", get(|| async { "Hello world" }))
+        .nest_service("/web", web_service)
         .layer(TraceLayer::new_for_http());
+
     let listener: TcpListener = tokio::net::TcpListener::bind("0.0.0.0:4242").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
