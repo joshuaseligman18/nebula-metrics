2a388551e8fb523dcd535b6b1f1036047dfeb5cd
Josh Seligman <joshuaseligman18@gmail.com>
2024-02-15 13:56:51 -0500

    Pruned the db

--- monitor/src/monitor/mod.rs
+++ monitor/src/monitor/mod.rs
@@ -31,6 +31,12 @@ impl Monitor {
         Ok(new_monitor)
     }
 
+    /// Constructor for the monitor specifically for testing
+    #[instrument(skip(pool))]
+    fn new_with_db_pool(pool: SqlitePool) -> Self {
+        Monitor { conn: pool }
+    }
+
     /// Initializes the database and verifies/cleans the pre-existing data
     #[instrument(skip(self))]
     pub async fn setup_init_data(&self) -> Result<(), NebulaError> {
@@ -44,6 +50,7 @@ impl Monitor {
         Ok(())
     }
 
+    /// Inserts new and updated informaiton in the database
     #[instrument(skip(self))]
     pub async fn update(&self, id: Id) {
         event!(Level::INFO, "Entering monitor update function");
@@ -66,5 +73,108 @@ impl Monitor {
             .expect("Should update disk data without error");
 
         event!(Level::INFO, "Exiting monitor update function");
+    }
+
+    /// Prunes the database by removing all outdated information
+    #[instrument(skip(self))]
+    pub async fn prune_db(&self, id: Id) {
+        event!(Level::INFO, "Entering database pruning");
+        // Tables with timestamp data: PROCSTAT, CPUSTAT, MEMORY, DISKSTAT
+        let cur_time: u64 = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .unwrap()
+            .as_secs();
+        // Prune any data that is from 3 hours ago or earlier
+        let three_hours_ago: u64 = cur_time - 60 * 60 * 3;
+
+        sqlx::query("DELETE FROM PROCSTAT WHERE TIMESTAMP < ?;")
+            .bind(three_hours_ago as i64)
+            .execute(&self.conn)
+            .await
+            .expect("Should be able to prune from PROCSTAT");
+
+        // Processes are just marked as dead, but can remove if dead for over
+        // 3 hours
+        sqlx::query("DELETE FROM PROCESS WHERE PID NOT IN (SELECT DISTINCT PID FROM PROCSTAT);")
+            .execute(&self.conn)
+            .await
+            .expect("Should be able to clear old processes");
+
+        sqlx::query("DELETE FROM MEMORY WHERE TIMESTAMP < ?;")
+            .bind(three_hours_ago as i64)
+            .execute(&self.conn)
+            .await
+            .expect("Should be able to prune from MEMORY");
+
+        sqlx::query("DELETE FROM DISKSTAT WHERE TIMESTAMP < ?;")
+            .bind(three_hours_ago as i64)
+            .execute(&self.conn)
+            .await
+            .expect("Should be able to prune from DISKSTAT");
+
+        event!(Level::INFO, "Exiting database pruning");
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::io;
+    use tracing::{span, Span};
+
+    #[sqlx::test(fixtures("pruneTest"))]
+    async fn test_update_disk_data(pool: SqlitePool) -> Result<(), NebulaError> {
+        let _ = tracing_subscriber::fmt()
+            .with_writer(io::stderr)
+            .with_max_level(Level::TRACE)
+            .try_init();
+
+        let prune_span: Span = span!(Level::TRACE, "prune-test");
+        let _guard = prune_span.enter();
+        let monitor: Monitor = Monitor::new_with_db_pool(pool.clone());
+
+        let _ = monitor.prune_db(prune_span.id().unwrap()).await;
+
+        assert_eq!(
+            sqlx::query("SELECT * FROM PROCESS;")
+                .fetch_all(&pool)
+                .await?
+                .len(),
+            1
+        );
+
+        assert_eq!(
+            sqlx::query("SELECT * FROM PROCSTAT;")
+                .fetch_all(&pool)
+                .await?
+                .len(),
+            1
+        );
+
+        assert_eq!(
+            sqlx::query("SELECT * FROM CPUSTAT;")
+                .fetch_all(&pool)
+                .await?
+                .len(),
+            1
+        );
+
+        assert_eq!(
+            sqlx::query("SELECT * FROM MEMORY;")
+                .fetch_all(&pool)
+                .await?
+                .len(),
+            1
+        );
+
+        assert_eq!(
+            sqlx::query("SELECT * FROM DISKSTAT;")
+                .fetch_all(&pool)
+                .await?
+                .len(),
+            1
+        );
+
+        Ok(())
     }
 }
--- monitor/src/monitor/process.rs
+++ monitor/src/monitor/process.rs
@@ -361,7 +361,8 @@ mod tests {
             .with_max_level(Level::TRACE)
             .try_init();
 
-        let cur_process_intermediate: ProcfsProcess = procfs::process::Process::myself()?.try_into()?;
+        let cur_process_intermediate: ProcfsProcess =
+            procfs::process::Process::myself()?.try_into()?;
         let cur_process: Process = cur_process_intermediate.into();
         sqlx::query("INSERT INTO PROCESS VALUES (?, ?, ?, ?, ?);")
             .bind(cur_process.pid)
@@ -407,7 +408,8 @@ mod tests {
             .try_init();
 
         // This will be an existing process that is already running
-        let cur_process_intermediate: ProcfsProcess = procfs::process::Process::myself()?.try_into()?;
+        let cur_process_intermediate: ProcfsProcess =
+            procfs::process::Process::myself()?.try_into()?;
         let cur_process: Process = cur_process_intermediate.into();
         sqlx::query("INSERT INTO PROCESS VALUES (?, ?, ?, ?, ?);")
             .bind(cur_process.pid)
