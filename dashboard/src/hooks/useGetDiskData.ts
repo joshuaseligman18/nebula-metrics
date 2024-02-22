8b64e44a68221a21724eaee98192865833f348e9
Trent325 <trgreen2002@gmail.com>
2024-02-21 20:08:40 -0500

    fixed route

--- dashboard/src/hooks/useGetDiskData.ts
+++ dashboard/src/hooks/useGetDiskData.ts
@@ -2,7 +2,7 @@ import { useQuery, UseQueryResult } from 'react-query';
 
 export const useGetDiskData = (): UseQueryResult<any, Error> => {
   return useQuery('GetDiskData', async () => {
-    const response = await fetch('http://127.0.0.1:4242/api/GetDiskData');
+    const response = await fetch('http://127.0.0.1:4242/api/disks');
     if (!response.ok) {
       throw new Error('Failed to fetch disk data');
     }
