0c43ea598aef82ede00f0c0b2b94ecdb0d680595
Josh Seligman <joshuaseligman18@gmail.com>
2024-03-08 14:09:59 -0500

    Fixed the expected disk response code in the test

--- server/src/main.rs
+++ server/src/main.rs
@@ -340,7 +340,7 @@ mod tests {
             )
             .await
             .unwrap();
-        assert_eq!(response.status(), StatusCode::INTERNAL_SERVER_ERROR);
+        assert_eq!(response.status(), StatusCode::OK);
 
         let res_string: String = String::from_utf8(
             response
