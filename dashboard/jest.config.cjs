b214059b479f4a74f7811b5a6f911fbbd9a7a7b9
Trent Green <70169861+Trent325@users.noreply.github.com>
2024-03-17 20:57:08 -0400

    getting farther

--- dashboard/jest.config.cjs
+++ dashboard/jest.config.cjs
@@ -8,7 +8,7 @@ module.exports = {
   testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
   testEnvironment: "jest-environment-jsdom",
-  setupFiles: ["<rootDir>/jest.setup.js"],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   transformIgnorePatterns: ["/node_modules/(?!ag-grid-community)", "!/node_modules/ag-grid-community/styles/"],
   moduleNameMapper: {
     "\\.(gif|ttf|eot|svg|png)$": "<rootDir>/test/__ mocks __/fileMock.js",
--- dashboard/test/HomePage.test.tsx
+++ dashboard/test/HomePage.test.tsx
@@ -1,6 +1,8 @@
 import HomePage from "../src/pages/HomePage";
 import { ModeProvider } from "../src/context/ModeContext";
 import { customRender } from "../src/queryClient/CustomRender";
+import LeaderboardBar from "../src/components/systemBar/LeaderBoardBar";
+import { act } from "@testing-library/react";
 
 jest.mock("../src/hooks/useGetAllProcesses", () => ({
   useAllProcesses: jest.fn(() => ({
@@ -12,19 +14,23 @@ jest.mock("../src/hooks/useGetAllProcesses", () => ({
 
 describe("HomePage Component Logic", () => {
   it("should render LeaderboardBar and ProcessChart components", async () => {
-    customRender(
-      <ModeProvider>
-        <HomePage />
-      </ModeProvider>
-    );
-   
+    
+
+    await act(async () => { // Use async/await for act
+      customRender(
+        <ModeProvider>
+          <HomePage />
+          <LeaderboardBar />
+        </ModeProvider>
+      );
+    });
   });
 
   // Add more test cases as needed to test different aspects of the logic
 
   // Test case for LeaderboardBar component
   it("should fetch CPU, Memory, and Disk data correctly", () => {
-    jest.mock("../src/hookshooks/useGetCpuData", () => ({
+    jest.mock("../src/hooks/useGetCpuData", () => ({
       useGetCpuData: jest.fn(() => ({
         data: [
           { cpu_core: 0, timestamp: 1, usage: 10 },
@@ -35,7 +41,7 @@ describe("HomePage Component Logic", () => {
       })),
     }));
     
-    jest.mock("../src/hookshooks/useGetMemoryData", () => ({
+    jest.mock("../src/hooks/useGetMemoryData", () => ({
       useGetMemoryData: jest.fn(() => ({
         data: [
           { timestamp: 1, total: 1024, free: 512, swap_total: 2048, swap_free: 1024 },
@@ -45,7 +51,7 @@ describe("HomePage Component Logic", () => {
       })),
     }));
     
-    jest.mock("../src/hookshooks/useGetDiskData", () => ({
+    jest.mock("../src/hooks/useGetDiskData", () => ({
       useGetDiskData: jest.fn(() => ({
         data: [
           { device_name: "disk1", timestamp: 1, available: 1024, used: 512 },
