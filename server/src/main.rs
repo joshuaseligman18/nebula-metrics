693797834e496722ff32ed702475a998189056f0
Trent325 <trgreen2002@gmail.com>
2024-03-28 20:05:35 -0400

    added new unit tests

--- server/src/main.rs
+++ server/src/main.rs
@@ -402,4 +402,84 @@ async fn test_api_processes(pool: SqlitePool) -> Result<(), sqlx::Error> {
 
         Ok(())
     }
+
+    #[sqlx::test(fixtures("apiTest"))]
+    async fn test_api_cpu_info_current(pool: SqlitePool) -> Result<(), sqlx::Error> {
+        let _ = tracing_subscriber::fmt()
+            .with_writer(io::stderr)
+            .with_max_level(Level::TRACE)
+            .try_init();
+
+        let app: Router = create_app(Some(pool)).await?;
+
+        let response: Response = app
+            .oneshot(
+                Request::builder()
+                    .uri("/api/cpu-info-current")
+                    .body(Body::empty())
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
+        assert_eq!(response.status(), StatusCode::OK);
+
+        let res_string: String = String::from_utf8(
+            response
+                .into_body()
+                .collect()
+                .await
+                .unwrap()
+                .to_bytes()
+                .to_vec(),
+        )
+        .expect("Should be able to convert to a string");
+
+        let res_vec: Vec<CpuInfo> = serde_json::from_str(&res_string)
+            .expect("Should be able to convert to a CPU info vec");
+
+        // Check if at least one item (CpuInfo) is returned
+        assert!(!res_vec.is_empty(), "Expected at least one item in the response");
+
+        Ok(())
+    }
+
+    #[sqlx::test(fixtures("apiTest"))]
+    async fn test_api_memory_current(pool: SqlitePool) -> Result<(), sqlx::Error> {
+        let _ = tracing_subscriber::fmt()
+            .with_writer(io::stderr)
+            .with_max_level(Level::TRACE)
+            .try_init();
+
+        let app: Router = create_app(Some(pool)).await?;
+
+        let response: Response = app
+            .oneshot(
+                Request::builder()
+                    .uri("/api/memory-current")
+                    .body(Body::empty())
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
+        assert_eq!(response.status(), StatusCode::OK);
+
+        let res_string: String = String::from_utf8(
+            response
+                .into_body()
+                .collect()
+                .await
+                .unwrap()
+                .to_bytes()
+                .to_vec(),
+        )
+        .expect("Should be able to convert to a string");
+
+        let res_vec: Vec<Memory> = serde_json::from_str(&res_string)
+            .expect("Should be able to convert to a memory vec");
+
+        // Check if at least one item (Memory) is returned
+        assert!(!res_vec.is_empty(), "Expected at least one item in the response");
+
+        Ok(())
+    }
 }
